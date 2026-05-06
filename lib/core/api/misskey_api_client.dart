1cb718558fae7ebd9a086d67efea2da5b22be65c
Cano <41741040+canoypa@users.noreply.github.com>
2026-05-06 16:43:27 +0900

    feat(core): add MisskeyApiClient with AuthInterceptor

---
+++ lib/core/api/misskey_api_client.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:mischie/core/api/auth_interceptor.dart';
+
+class MisskeyApiClient {
+  MisskeyApiClient({required String host, required Future<String?> Function() getToken})
+      : _dio = Dio(
+          BaseOptions(
+            baseUrl: 'https://$host/api/',
+            contentType: 'application/json',
+          ),
+        ) {
+    _dio.interceptors.add(AuthInterceptor(getToken));
+  }
+
+  final Dio _dio;
+
+  Future<Map<String, dynamic>> post(
+    String path, {
+    Map<String, dynamic>? body,
+  }) async {
+    final response = await _dio.post<Map<String, dynamic>>(
+      path,
+      data: body ?? {},
+    );
+    return response.data!;
+  }
+}
